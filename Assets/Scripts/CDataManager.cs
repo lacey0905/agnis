90b0870b29f6244f2b82bec10ee2a02c31e9a86c
username <user@email.mail>
2020-01-07 15:34:30 +0900

    0107

---
+++ Assets/Scripts/CDataManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CDataManager : MonoBehaviour
+{
+    private double myGold;
+    private int myRuby;
+    private int currentStage;
+    private int currentMonsterStep;
+    private int maxMonsterStep;
+    private int myTapUpgradeLevel;
+
+    public static CDataManager instance;
+
+    private void Awake()
+    {
+        CDataManager.instance = this;
+    }
+
+    // 골드
+    public double GetMyGold()
+    {
+        return myGold;
+    }
+    public void AddMyGold(double gold)
+    {
+        myGold += gold;
+    }
+
+    // 루비
+    public int GetMyRuby()
+    {
+        return myRuby;
+    }
+    public void AddMyRuby(int ruby)
+    {
+        myRuby += ruby;
+    }
+
+    // 스테이지
+    public int GetCurrentStage()
+    {
+        return currentStage;
+    }
+    public void SetCurrentStage(int stage)
+    {
+        currentStage = stage;
+    }
+
+    // 업그레이드
+    public int GetMyTapUpgradeLevel()
+    {
+        return myTapUpgradeLevel;
+    }
+    public void SetMyTapUpgradeLevel(int level)
+    {
+        myTapUpgradeLevel = level;
+    }
+
+    /*
+        골드
+        루비
+        스테이지
+        몬스터 스탭    
+
+        탭 업글
+        #장비 번호 + 업
+    */
+
+}
---
+++ Assets/Scripts/CMonsterManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMonsterManager : MonoBehaviour
+{
+    private double maxMonsterHealth;
+    private double currentMonsterHealth;
+
+    public void Create()
+    {
+        // 스테이지 가져오기
+        int currentStage = CDataManager.instance.GetCurrentStage();
+
+        // 몬스터 체력 수식 예정
+        maxMonsterHealth = currentStage * 10d;
+        currentMonsterHealth = maxMonsterHealth;
+
+        // 몹 생성 및 UI 갱신
+    }
+
+    public void Attack(double damage)
+    {
+        currentMonsterHealth -= damage;
+
+        // 몬스터 다이
+        if(currentMonsterHealth <= 0)
+        {
+            // 체력이 -가 되지 않게
+            currentMonsterHealth = 0;
+
+            // 몬스터 Die 기능 실행
+
+        }
+        else
+        {
+            // 몬스터 맞는 효과나 이런거
+        }
+    }
+
+    public void Die()
+    {
+        // 몬스터 다이 효과
+
+        // 몬스터 새로 생성
+
+        // 돈 생성
+    }
+
+}
