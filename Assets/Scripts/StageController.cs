9eef79f34ddbb6c62ee71efac028c0e33e538b4a
DESKTOP-6QHKM6B\LACEY <lacey0905@naver.com>
2020-01-30 11:19:17 +0900

    싱글톤

---
+++ Assets/Scripts/StageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+
+    private int currentStage;
+
+
+    public int GetCurrentStage()
+    {
+        return currentStage;
+    }
+
+
+
+
+}
